817cc79bccd763faf0d176715c730be7a607a58f
Arshid <arshid@Arshids-MacBook-Air.local>
2025-09-03 12:29:35 +0530

    v1.2.0

--- resources/views/filament/pages/settings.blade.php
+++ resources/views/filament/pages/settings.blade.php
@@ -1,12 +1,12 @@
 <x-filament::page>
-	<x-filament-panels::form :wire:key="$this->getId() . '.forms.' . $this->getFormStatePath()" wire:submit="save">
+	<form wire:submit="save">
 		{{ $this->form }}
         @if (session()->has('success'))
             <p class="mt-4 text-green-500">{{ session('success') }}</p>
         @endif
-        <x-filament::button size="sm" wire:click='save' class="">
+        <x-filament::button size="sm mt-5" wire:click='save' class="">
             Save
         </x-filament::button>
-	</x-filament-panels::form>    
+	</form>    
 
 </x-filament::page>
